9b842bc4f572fb2539e71819cf7b65dec2f3c763
Pr45H4nt <111138706+Pr45H4nt@users.noreply.github.com>
2023-02-15 09:16:24 +0545

    Update get_domain.py

--- get_domain.py
+++ get_domain.py
@@ -13,3 +13,14 @@ def get_domain_name(url):
         url = url[:url.index('/')]
 
     return url
+
+
+#using regular expression
+def get_domain_name(url):
+    import re
+    pattern = r"(https?://)?(www\.)?([a-zA-Z0-9-]+)(\.[a-zA-Z]{2,}){1,2}"
+    match = re.search(pattern, url)
+    if match:
+        return match.group(3) + match.group(4)
+
+    return None
